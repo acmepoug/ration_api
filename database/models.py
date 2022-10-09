9d81766de39c4ee36f4672cdb2e4c21b0c9d64ee
acmepoug <115389098+acmepoug@users.noreply.github.com>
2022-10-09 07:31:41 +0000

    Create Meal DB Model

--- database/models.py
+++ database/models.py
@@ -21,3 +21,29 @@ t_connect_dish_portion = Table(
     Column("portion_id", ForeignKey("portions.id"), primary_key=True, ),
 )
 
+class Meal(BaseModel):
+    __tablename__ = "meals"
+
+    name = Column(String(100))
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, )
+    user = relationship("User", back_populates="meals", )
+
+    dishes = relationship(
+        "Dish", secondary=t_connect_meal_dish, back_populates="meals", lazy="dynamic",
+    )
+
+    def add_dish(self, dish: Dish) -> None:
+        if dish not in self.dishes.all():
+            self.dishes.append(dish)
+
+    def add_portion_as_dish(self, portion: Portion) -> None:
+        new_dish = Dish(name=portion.title, user_id=self.user_id)
+        new_dish.add_portion(portion)
+        self.add_dish(new_dish)
+
+    def add_to_meal(self, item: Union[Dish, Portion]) -> None:
+        if isinstance(item, Dish):
+            self.add_dish(item)
+        elif isinstance(item, Portion):
+            self.add_portion_as_dish(item)
